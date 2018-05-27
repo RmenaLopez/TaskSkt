ca4457192cd5294fb652704b4d0112ba85e6ea62
Rafael Mena <rmena@nearsoft.com>
2018-05-27 15:31:53 -0700

    remove duplicate POJO in microservice

--- commonLibrary/src/main/java/com/skt/finaltask/commonLibrary/model/User.java
+++ commonLibrary/src/main/java/com/skt/finaltask/commonLibrary/model/User.java
@@ -1,11 +1,40 @@
 package com.skt.finaltask.commonLibrary.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.NamedStoredProcedureQueries;
+import javax.persistence.NamedStoredProcedureQuery;
+import javax.persistence.StoredProcedureParameter;
+import javax.persistence.ParameterMode;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 import java.io.Serializable;
 
+@Entity
+@Table( name = "user_tbl")
+@NamedStoredProcedureQueries({
+        @NamedStoredProcedureQuery(name = "getAllUsers",
+                procedureName = "find_all_users", resultClasses = User.class),
+        @NamedStoredProcedureQuery(name = "addUser",
+                procedureName = "insert_new_user",
+                parameters = {
+                        @StoredProcedureParameter(name = "name", type = String.class, mode = ParameterMode.IN),
+                        @StoredProcedureParameter(name = "age", type = Integer.class, mode = ParameterMode.IN)
+                })
+})
 public class User implements Serializable{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "age")
     private int age;
     public User() {
     }
